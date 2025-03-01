72206b05f6a714a35bdc2b73b6dbddcc43b16c50
Samantha <samantha.c.alvarenga@gmail.com>
2025-02-28 21:56:59 -0300

    configurando as rotas da aplicação

--- src/routes.php
+++ src/routes.php
@@ -4,5 +4,24 @@ use core\Router;
 $router = new Router();
 
 $router->get('/', 'HomeController@index');
-$router->get('/sobre/{nome}', 'HomeController@sobreP');
-$router->get('/sobre', 'HomeController@sobre');
+
+//rotas Cadastrar Usuarios
+$router->get('/cadUsuario', 'HomeController@sobre');
+$router->post('/cadUsuario','HomeController@');
+
+//rotas Cadastrar Doação
+$router->get('/cadDoacao', 'HomeController@sobre');
+$router->post('/cadDoacao', 'HomeController@sobre');
+
+//rotas das Vendas
+$router->get('/venda', 'HomeController@sobre');
+$router->post('/venda','HomeController@sobre');
+
+//Saldo
+$router->get('/verSaldo', 'HomeController@sobre');
+
+
+
+
+/* $router->get('/sobre/{nome}', 'HomeController@sobreP');
+$router->get('/sobre', 'HomeController@sobre'); */
